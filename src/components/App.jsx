5c0f058346f7dc0e4b23fbac6dcc2b5500ebbda2
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2024-02-22 08:37:02 +0800

    commit-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -78,5 +78,3 @@ export const App = () => {
     </div>
   );
 };
-
-export { App };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,7 +3,7 @@ import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 
 import css from './ImageGallery.module.css';
 
-const ImageGallery = ({ images, onImageClick }) => {
+export const ImageGallery = ({ images, onImageClick }) => {
   return (
     <ul className={css.gallery}>
       {images.map(image => (
@@ -17,10 +17,10 @@ ImageGallery.propTypes = {
   images: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
-      // Add other properties of your 'image' object here
+      url: PropTypes.string.isRequired, // Add other properties of your 'image' object here
+      description: PropTypes.string,
+      // ... other properties
     })
   ).isRequired,
   onImageClick: PropTypes.func.isRequired,
 };
-
-export { ImageGallery };
